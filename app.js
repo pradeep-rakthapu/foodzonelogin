9799247511c1a6dc3e98bf7628eef957a1cdde95
Pradeep22 <pradeepgoud.rpg@gmail.com>
2022-06-24 12:32:53 +0530

    login

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const db = require('./db');
+const port = process.env.PORT || 8080;
+const cors = require('cors');
+app.use(cors());
+
+const AuthController = require('./controller/authController');
+app.use('/api/auth', AuthController);
+
+app.listen(port,() => {
+    console.log(`Running on port ${port}`)
+})
---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://pradeepgoud:qVCrPh8XVbIq1bkz@cluster0.tojfe.mongodb.net/test?retryWrites=true&w=majority')
